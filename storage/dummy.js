7d2cfa1222a17af8980b67a6eb6f7dd5c31c1ce2
dgsaltarin <dgsaltarin@gmail.com>
2021-11-08 20:02:52 -0500

    initial commit

---
+++ storage/dummy.js
@@ -0,0 +1,27 @@
+const db = {
+  user: [{ id: '1', name: 'Daniel' }],
+};
+
+async function list(table) {
+  return db[table];
+}
+
+async function get(table, id) {
+  let collection = await list(table);
+  return collection.filter((item) => item.id === id)[0] || null;
+}
+
+async function upsert(table, data) {
+  db[collection].push(data);
+}
+
+async function remove(table, id) {
+  return true;
+}
+
+module.exports = {
+  list,
+  get,
+  upsert,
+  remove,
+};
